5c82b8f941f7bb71e0f8c5a7e5dd068ee2b95554
Muhammad Irva <48151470+justirva09@users.noreply.github.com>
2021-04-01 23:24:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {React, Component, BrowserRouter, Switch, Route} from 'libraries';
 
 import { route } from 'routes';
 
+// https://rawcdn.githack.com/justirva09/recipe-apps/eb47281f3edb19393d6cc219dfc2c182c517fee4/src/assets/dummy/resep.json
+
 class App extends Component {
   render() {
     return(
